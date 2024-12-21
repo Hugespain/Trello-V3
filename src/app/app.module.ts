74182a48dd7bd1e0fc56822d982888f7c9a43a99
Hugespain <diegosilva3795@gmail.com>
2024-12-21 11:46:56 +0100

    Antes de crear el CRUD y el page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskServiceComponent } from './features/tasks/services/task-service/task-service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskServiceComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/features/tasks/services/tak.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../interfaces/task.interface';
+import { environments } from '../../../../environments/environments';
+
+@Injectable({providedIn: 'root'})
+export class ServiceNameService {
+
+  private baseUrl: string = environments.baseUrl;
+
+
+  constructor(private http: HttpClient) { }
+
+  getHeroes(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.baseUrl}/tasks`);
+  }
+}
