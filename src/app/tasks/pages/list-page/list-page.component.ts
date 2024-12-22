b074b4550355f67210a66166a99ccd2b40c38a53
Hugespain <diegosilva3795@gmail.com>
2024-12-22 13:53:03 +0100

    Tareas cargadas en list y mostradas en el layout

--- src/app/tasks/pages/list-page/list-page.component.ts
+++ src/app/tasks/pages/list-page/list-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Estado, Task } from '../../interfaces/task.interface';
+import { TasksService } from '../../services/task.service';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
-  styleUrl: './list-page.component.css'
+  styleUrls: ['./list-page.component.css']
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
 
+  public tasksPendientes: Task[] = [];
+  public tasksEnProgreso: Task[] = [];
+  public tasksTerminadas: Task[] = [];
+
+  constructor(private taskService: TasksService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe(tasks => {
+      this.tasksPendientes = tasks.filter(task => task.estado === Estado.Pendiente);
+      this.tasksEnProgreso = tasks.filter(task => task.estado === Estado.Enprogreso);
+      this.tasksTerminadas = tasks.filter(task => task.estado === Estado.Terminada);
+    });
+  }
 }
--- src/app/tasks/services/task.service.ts
+++ src/app/tasks/services/task.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
+import { environments } from '../../../environments/environments';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Task } from '../interfaces/task.interface';
-import { environments } from '../../../environments/environments';
 
 @Injectable({providedIn: 'root'})
 export class TasksService {
-
   private baseUrl: string = environments.baseUrl;
 
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getTareas(): Observable<Task[]> {
-    return this.http.get<Task[]>(`${this.baseUrl}/tasks`);
+  getTasks(): Observable<Task[]> {
+  return this.http.get<Task[]>(`${this.baseUrl}/tasks`);
   }
 }
