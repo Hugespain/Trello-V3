e27303e361391036f5c1966d81d49354f415256f
Hugespain <diegosilva3795@gmail.com>
2024-12-27 12:33:12 +0100

    Drag and Drop completo implementado

--- src/app/tasks/pages/list-page/list-page.component.ts
+++ src/app/tasks/pages/list-page/list-page.component.ts
@@ -26,6 +26,48 @@
 
 
 
+
+// import { Component, OnInit } from '@angular/core';
+// import { Estado, Task } from '../../interfaces/task.interface';
+// import { TasksService } from '../../services/task.service';
+// import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+// @Component({
+//   selector: 'app-list-page',
+//   templateUrl: './list-page.component.html',
+//   styleUrls: ['./list-page.component.css']
+// })
+// export class ListPageComponent implements OnInit {
+
+//   public tasksPendientes: Task[] = [];
+//   public tasksEnProgreso: Task[] = [];
+//   public tasksTerminadas: Task[] = [];
+
+//   public Estado = Estado;
+
+//   constructor(private taskService: TasksService) {}
+
+//   ngOnInit(): void {
+//     this.taskService.getTasks().subscribe(tasks => {
+//       this.tasksPendientes = tasks.filter(task => task.estado === Estado.Pendiente);
+//       this.tasksEnProgreso = tasks.filter(task => task.estado === Estado.Enprogreso);
+//       this.tasksTerminadas = tasks.filter(task => task.estado === Estado.Terminada);
+//     });
+//   }
+
+//   drop(event: CdkDragDrop<Task[]>, newEstado: Estado): void {
+//     if (event.previousContainer === event.container) {
+//       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+//     } else {
+//       const task = event.previousContainer.data[event.previousIndex];
+//       task.estado = newEstado;
+//       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+//       this.taskService.updateTask(task).subscribe();
+//     }
+//   }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 import { Estado, Task } from '../../interfaces/task.interface';
 import { TasksService } from '../../services/task.service';
@@ -61,7 +103,7 @@ export class ListPageComponent implements OnInit {
       const task = event.previousContainer.data[event.previousIndex];
       task.estado = newEstado;
       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
-      this.taskService.updateTask(task).subscribe();
+      this.taskService.updateTask(task).subscribe(); // Actualiza el backend
     }
   }
 }
--- src/app/tasks/services/task.service.ts
+++ src/app/tasks/services/task.service.ts
@@ -45,11 +45,23 @@ export class TasksService {
 
   constructor(private http: HttpClient) {}
 
+  // Obtener todas las tareas
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(`${this.baseUrl}/tasks`);
   }
 
+  // Crear una nueva tarea
+  createTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(`${this.baseUrl}/tasks`, task);
+  }
+
+  // Actualizar una tarea existente
   updateTask(task: Task): Observable<Task> {
     return this.http.put<Task>(`${this.baseUrl}/tasks/${task.id}`, task);
+  }
+
+  // Eliminar una tarea
+  deleteTask(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/tasks/${id}`);
   }
 }
