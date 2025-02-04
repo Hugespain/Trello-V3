224f8c0eb3f1de31a90ad4e35466f8bde25993b9
Hugespain <diegosilva3795@gmail.com>
2025-02-04 11:13:39 +0100

    Antes de iniciar componente login

--- src/app/tasks/pages/layout-page/layout-page/layout-page.component.ts
+++ src/app/tasks/pages/layout-page/layout-page/layout-page.component.ts
@@ -10,8 +10,8 @@ export class LayoutPageComponent {
 
   public sidebarItems = [
     { label: 'SIDEBAR.LISTADO', icon: 'label', url: '' },
-    { label: 'SIDEBAR.ADD_TASK', icon: 'add', url: 'new' },
-    { label: 'SIDEBAR.EDIT_DELETE_TASK', icon: 'edit', url: 'edit' },
+    // { label: 'SIDEBAR.ADD_TASK', icon: 'add', url: 'new' },
+    // { label: 'SIDEBAR.EDIT_DELETE_TASK', icon: 'edit', url: 'edit' },
     { label: 'SIDEBAR.CATEGORIES', icon: 'category', url: 'categorias' },
     { label: 'SIDEBAR.PEOPLE', icon: 'group_add', url: 'personas' }
   ];
